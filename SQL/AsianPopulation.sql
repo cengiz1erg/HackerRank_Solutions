078dcf54bd1e1579f743149a3a86c912fe34de7e
cengiz1erg <73190632+cengiz1erg@users.noreply.github.com>
2021-01-18 15:42:19 +0300

    Update AsianPopulation.sql

--- SQL/AsianPopulation.sql
+++ SQL/AsianPopulation.sql
@@ -1,13 +1,20 @@
 SELECT SUM(POPULATION)
 FROM CITY
 WHERE COUNTRYCODE IN (SELECT CODE FROM COUNTRY WHERE CONTINENT = 'Asia');
+/*--------------USING JOIN--------------
+SELECT SUM(CI.POPULATION)
+FROM CITY CI
+INNER JOIN COUNTRY CO ON CI.COUNTRYCODE = CO.CODE
+WHERE CONTINENT = 'Asia'
+*/
 
-/* ----- HATALI İLK HAL -----
+/* -------------WRONG SOLUTION----------
 SELECT SUM(POPULATION)
 FROM CITY
 WHERE COUNTRYCODE = (SELECT CODE FROM COUNTRY WHERE CONTINENT = 'Asia');
 */ 
-/* ----- SONUÇ -----
+
+/* -----------------SONUÇ---------------
 altsorgunun çıktısı tek değer ise "=" kullan
 birden fazla çıktı varsa recursive gerekli ve "IN" kullan 
 */
